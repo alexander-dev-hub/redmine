5edad62cb79044a0c00e6a4d097c2558de002e11
Alexander <wangming1126@outlook.com>
2019-09-05 02:17:38 +0700

    first commit

---
+++ lib/plugins/open_id_authentication/init.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: false
+
+if Rails.version < '3'
+  config.gem 'rack-openid', :lib => 'rack/openid', :version => '>=0.2.1'
+end
+
+require 'open_id_authentication'
+
+config.middleware.use OpenIdAuthentication
+
+config.after_initialize do
+  OpenID::Util.logger = Rails.logger
+  ActionController::Base.send :include, OpenIdAuthentication
+end
